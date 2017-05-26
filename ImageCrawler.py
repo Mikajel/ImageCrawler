72d3a31baf11a946a49ae6823bb6d776768f8c33
Mikajel <fasanekmichal@gmailcom>
2017-05-26 18:08:54 +0200

    Added duplicate url and img control

--- ImageCrawler.py
+++ ImageCrawler.py
@@ -2,18 +2,22 @@ import urllib.request, urllib.error
 from bs4 import BeautifulSoup
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
+from re import search
 
-base_url = 'http://exponea.com'
+domain_regex_front = 'https://exponea.com'
+base_url = 'https://exponea.com'
 urls = [base_url]
 checked_urls = []
 images = []
 
+validate = URLValidator()
 
-def process_url(url: str) -> []:
+
+def process_url(url: str, validate: URLValidator) -> []:
 
     print('Processing url: %s' % url)
 
-    validate = URLValidator()
+
     try:
         validate(url)
     except ValidationError:
@@ -40,11 +44,11 @@ while len(urls) > 0:
     actual_url = urls[0]
     checked_urls.append(urls.pop(0))
 
-    new_urls, new_images = process_url(actual_url)
+    new_urls, new_images = process_url(actual_url, validate)
 
     # TODO: check for duplicities
-    urls += new_urls
-    images += new_images
+    urls += [url for url in new_urls if url not in urls]
+    images += [image for image in new_images if image not in images]
 
     pass
 
